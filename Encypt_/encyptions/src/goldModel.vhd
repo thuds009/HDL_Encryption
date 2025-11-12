330a94b0d0663a4fbe65d9b500ef3db0f4266030
thuds009 <thuds009@odu.edu>
2025-11-11 19:19:31 -0500

    updated ports

--- Encypt_/encyptions/src/goldModel.vhd
+++ Encypt_/encyptions/src/goldModel.vhd
@@ -3,8 +3,18 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity goldModel is 
-	port (clock, reset, mode_select, cbc_enabled, start: in std_logic;
-		data_in, key_in, iv_in: in std_logic_vector(127 downto 0);
-	data_out: out std_logic_vector(127 downto 0);
-		done: out std_logic);
-end entity;
+	port (
+	clock 		:in std_logic;
+	reset		:in std_logic;
+	mode_select	:in std_logic;
+	cbc_enabled	:in std_logic;
+	start		:in std_logic;
+	
+	data_in		:in std_logic_vector(127 downto 0);
+	key_in		:in std_logic_vector(127 downto 0);
+	iv_in		:in std_logic_vector(127 downto 0);
+	
+	data_out	:out std_logic_vector(127 downto 0);
+	done 		:out std_logic
+	);
+end entity goldModel;
